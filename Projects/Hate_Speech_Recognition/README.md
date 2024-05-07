bcbfd1567ac58c4c691a84004f11317c3e4826fd
arpanira <archana.paniraj@gmail.com>
2024-05-07 12:12:11 +0530

    Add new README

---
+++ Projects/Hate_Speech_Recognition/README.md
@@ -0,0 +1,15 @@
+# HATE SPEECh REGONITION
+
+# How to run?
+
+```bash
+conda create -n Hate_Speech_recognition python=3.8 -y
+```
+
+```bash
+conda activate Hate_Speech_recognition
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ Projects/Hate_Speech_Recognition/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Hate_Speech_recognition",
+    version="0.0.1",
+    author="Archana",
+    author_email="archana.paniraj@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
